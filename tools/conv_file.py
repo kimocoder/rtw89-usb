81cf34025a61104f5d4c8211cfaa9b5c77dc0eb2
Sourcery AI <>
2023-04-24 01:36:34 +0000

    'Refactored by Sourcery'

--- tools/conv_file.py
+++ tools/conv_file.py
@@ -3,26 +3,24 @@
 import sys
 
 def parse(filename):
-	fo = open(filename, "r")
-	while True:
-		line = fo.readline()
-		if not line:
-			break;
+	with open(filename, "r") as fo:
+		while True:
+			line = fo.readline()
+			if not line:
+				break;
 
-		if line.startswith('//'):
-			continue
+			if line.startswith('//'):
+				continue
 
-		items = line.strip("\r\n").split('\t')
-		if len(items) < 2:
-			continue
-		if not items[0]:
-			continue
-		if not items[1]:
-			continue
+			items = line.strip("\r\n").split('\t')
+			if len(items) < 2:
+				continue
+			if not items[0]:
+				continue
+			if not items[1]:
+				continue
 
-		print('\t%s, %s,' % (items[0].strip(), (items[1].split())[0].strip()))
-	fo.close()
+			print('\t%s, %s,' % (items[0].strip(), (items[1].split())[0].strip()))
 
-if __name__ == "__main__":
-	if len(sys.argv) == 2:
-		parse(sys.argv[1])
+if __name__ == "__main__" and len(sys.argv) == 2:
+	parse(sys.argv[1])
